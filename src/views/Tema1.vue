f8cccdb7023d48fa44866133a4b522f31a8ad25e
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2023-08-01 13:13:53 -0500

    Avance 7

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -243,7 +243,7 @@
           h5 Figura 1.
           span Estructura de la vía
         figure(data-aos="zoom-in")
-          img(src='@/assets/curso/tema1/img18.svg' alt="Describe la estructura de la vía con el fin de tenerse en cuenta para el proceso de barrido")
+          img(src='@/assets/curso/tema1/img18.svg' alt="Describe la estructura de la vía, con el fin de tenerse en cuenta para el proceso de barrido. Muestra la cuneta, la calzada, el bode de la acera y la acera.")
     
     p.mb-5 La importancia de conocer esta estructura radica en el procedimiento para realizar el barrido:
 
--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -7,7 +7,7 @@
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
-          img(src="@/assets/curso/temas/sintesis.svg", alt="Síntesis que resume los temas abordados dentro del componente formativo")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="Muestra la síntesis del componente formativo, caracterización del servicio público domiciliario de aseo, el barrido y lavado de las vías y micro macro rutas.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
